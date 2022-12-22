a5d3cc8566daed74c393672ca72e3ea676343eea
Dongmin Ahn <eamon3481@naver.com>
2022-12-22 17:08:29 +0900

    feat : react query 사용을 위한 QueryClientProvider 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <RecoilRoot>
-      <Component {...pageProps} />;
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />;
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
