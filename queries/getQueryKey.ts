83b028dc4e738696f25fc3b5544c4c2a4c12ee0a
Dongmin Ahn <eamon3481@naver.com>
2022-12-22 17:15:37 +0900

    feat : query key 관리를 위한 getQueryKey util 함수 추가

---
+++ queries/getQueryKey.ts
@@ -0,0 +1,11 @@
+type QueryList = {
+  FARMS: undefined;
+};
+
+const getQueryKey = <T extends keyof QueryList>(
+  ...[key, params]: undefined extends QueryList[T] ? [T] : [T, QueryList[T]]
+) => {
+  return params ? [key, params] : [key];
+};
+
+export default getQueryKey;
