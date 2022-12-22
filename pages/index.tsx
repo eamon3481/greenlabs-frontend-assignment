9b260bac948f7907548086facac030aacf41bef1
Dongmin Ahn <eamon3481@naver.com>
2022-12-22 15:17:20 +0900

    feat : 접근성을 위해 label 과 form tag 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ScreenWrap, Container, Input, Button } from "components";
+import React from 'react';
+import { ScreenWrap, Container, Input, Button } from 'components';
 
 //TODO: Q1-1 로그인 상태 관리
 // 상태관리 라이브러리 (context, redux, recoil 등) 을 활용해서 로그인 상태를 관리하는 기능을 개발 해주세요
@@ -7,18 +7,20 @@ import { ScreenWrap, Container, Input, Button } from "components";
 // 로그인이 완료되면 /home 라우터로 이동해야합니다.
 
 const Login = () => {
-  return (
-    <Container>
-      <ScreenWrap>
-        <div className="w-[90%] flex flex-col gap-2">
-          <h1 className="font-bold text-xl">그린랩스 농장관리 서비스</h1>
-          <Input type="text" name="id" placeholder="아이디를 입력하세요" />
-          <Input type="text" name="name" placeholder="이름을 입력하세요" />
-          <Button>로그인</Button>
-        </div>
-      </ScreenWrap>
-    </Container>
-  );
+ return (
+  <Container>
+   <ScreenWrap>
+    <form className="w-[90%] flex flex-col gap-2">
+     <h1 className="font-bold text-xl">그린랩스 농장관리 서비스</h1>
+     <label htmlFor="id">아이디</label>
+     <Input id="id" type="text" name="id" placeholder="아이디를 입력하세요" />
+     <label htmlFor="name">이름</label>
+     <Input id="name" type="text" name="name" placeholder="이름을 입력하세요" />
+     <Button>로그인</Button>
+    </form>
+   </ScreenWrap>
+  </Container>
+ );
 };
 
 export default Login;
