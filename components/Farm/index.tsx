80d7f8e53e845c5b5964d36068db0bddb45f4dbb
Dongmin Ahn <eamon3481@naver.com>
2022-12-22 17:23:15 +0900

    feat : farm list 랜더링

--- components/Farm/index.tsx
+++ components/Farm/index.tsx
@@ -1,4 +1,6 @@
+import useFarms from "queries/useFarms";
 import React from "react";
+import FarmList from "./FarmList";
 // import type { ProductionType, FarmsType } from "libs/type";
 
 const Farm = () => {
@@ -11,8 +13,17 @@ const Farm = () => {
   /* TODO: Q3-2 화면 조작
   - 랜더링 된 컴포넌트에서 하우스를 동작시키는 함수를 작성해 주세요 역시 예시 이미지를 확인 하세요.
   */
+  const { data, isLoading } = useFarms();
 
-  return <div className="px-2 flex flex-col gap-2"></div>;
+  if (!data || isLoading) return null;
+
+  return (
+    <div className="px-2 flex flex-col gap-2">
+      {data.farms.map((farm) => (
+        <FarmList {...farm} productionTotal={0} HouseActive={() => {}} />
+      ))}
+    </div>
+  );
 };
 
 export default Farm;
