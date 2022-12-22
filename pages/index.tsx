9d9538a1cf2ee12fb32038e35873ca0267191fea
Dongmin Ahn <eamon3481@naver.com>
2022-12-22 15:20:25 +0900

    feat : handleLoginSubmit 함수 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { FormEventHandler } from 'react';
 import { ScreenWrap, Container, Input, Button } from 'components';
+import { useSetRecoilState } from 'recoil';
+import { userAtom } from 'stores';
 
 //TODO: Q1-1 로그인 상태 관리
 // 상태관리 라이브러리 (context, redux, recoil 등) 을 활용해서 로그인 상태를 관리하는 기능을 개발 해주세요
@@ -7,10 +9,19 @@ import { ScreenWrap, Container, Input, Button } from 'components';
 // 로그인이 완료되면 /home 라우터로 이동해야합니다.
 
 const Login = () => {
+ const handleLoginSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+  e.preventDefault();
+  const { id, name } = e.target as typeof e.target & {
+   id: { value: string };
+   name: { value: string };
+  };
+
+  console.log(id.value, name.value);
+ };
  return (
   <Container>
    <ScreenWrap>
-    <form className="w-[90%] flex flex-col gap-2">
+    <form onSubmit={handleLoginSubmit} className="w-[90%] flex flex-col gap-2">
      <h1 className="font-bold text-xl">그린랩스 농장관리 서비스</h1>
      <label htmlFor="id">아이디</label>
      <Input id="id" type="text" name="id" placeholder="아이디를 입력하세요" />
