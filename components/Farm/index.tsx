5b7f02cbf2fc38f7de57440ad149e28a81fa565d
Dongmin Ahn <eamon3481@naver.com>
2022-12-22 18:01:56 +0900

    chore : TODO: Q3-1 화면 조작 완료 및 주석제거

--- components/Farm/index.tsx
+++ components/Farm/index.tsx
@@ -4,9 +4,6 @@ import FarmList from "./FarmList";
 import { getProductionTotal } from "utils";
 
 const Farm = () => {
-  /* TODO: Q3-1 화면 조작
-  - 호출 받은 데이터중 annualProduction 은 월별 생산량을 가지고 있습니다. 월별 생산량의 총 합을 구하는 함수를 작성해서 
-    계산된 값을 FarmList 컴포넌트의 productionTotal Props 로 내려주세요
   /* TODO: Q3-2 화면 조작
   - 랜더링 된 컴포넌트에서 하우스를 동작시키는 함수를 작성해 주세요 역시 예시 이미지를 확인 하세요.
   */
