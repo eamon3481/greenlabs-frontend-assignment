903815243868fd3a05bc031cfe3609410011bb65
Dongmin Ahn <eamon3481@naver.com>
2022-12-22 17:30:49 +0900

    feat : post 메서드로 farm 정보 보내는 request 함수 작성

--- apis/request.ts
+++ apis/request.ts
@@ -1,5 +1,12 @@
 import { FarmsType } from "libs/type";
 import { API, call } from "./API";
 
+type GetFarmListResponseType = { farms: FarmsType[] };
+
 export const getFarmList = () =>
-  call<{ farms: FarmsType[] }>(() => API.get("/api/farm"));
+  call<GetFarmListResponseType>(() => API.get("/api/farm"));
+
+export type AddFarmRequestType = { name: string; crop: string };
+
+export const addFarm = (farm: AddFarmRequestType) =>
+  call(() => API.post("/api/addfarm", farm));
