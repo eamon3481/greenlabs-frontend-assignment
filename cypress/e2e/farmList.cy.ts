cc8b912493f90a091668703fadf073f12e955e15
Dongmin Ahn <eamon3481@naver.com>
2022-12-25 21:48:54 +0900

    test : farmList 관련 테스트 작성

---
+++ cypress/e2e/farmList.cy.ts
@@ -0,0 +1,100 @@
+import { ID, NAME } from "cypress/support/commands";
+import { farms } from "./mockData";
+
+describe("/home 에 접속하면 /api/farm 을 호출한 뒤에, FarmList 를 보여줍니다.", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("input[name='name']").type(NAME);
+    cy.get("input[name='id']").type(ID);
+    cy.get("button[type='submit']").click();
+    cy.intercept("GET", "/api/farm", { farms }).as("getFarmList");
+  });
+
+  it("/api/farm 을 호출합니다.", () => {
+    cy.wait("@getFarmList");
+  });
+
+  it("내려온 데이터에 따라서 FarmList 를 보여줍니다.", () => {
+    cy.wait("@getFarmList");
+    farms.forEach((farm) => {
+      cy.contains(farm.name).should("exist");
+    });
+  });
+
+  it("houseList 가 있다면 houseList 를 보여줍니다. 가동중이면 초록색 중지 중이면 빨간색으로 보여줍니다.", () => {
+    cy.wait("@getFarmList");
+    farms.forEach((farm) => {
+      if (!farm.houses) {
+        return;
+      }
+      cy.contains(farm.name)
+        .parent()
+        .parent()
+        .within(() => {
+          farm.houses.forEach((house) => {
+            cy.contains(house.name).should("exist");
+            if (house.active) {
+              cy.contains(house.name).should(
+                "have.css",
+                "backgroundColor",
+                "rgb(122, 234, 156)"
+              );
+            } else {
+              cy.contains(house.name).should(
+                "have.css",
+                "backgroundColor",
+                "rgb(255, 130, 105)"
+              );
+            }
+          });
+        });
+    });
+  });
+
+  it(" 월별 생산량의 총 합을 연간 생산량으로 보여줍니다.", () => {
+    cy.wait("@getFarmList");
+    farms.forEach((farm) => {
+      const total = farm.annualProduction.reduce(
+        (acc, cur) => acc + cur.Production,
+        0
+      );
+
+      cy.contains(farm.name)
+        .parent()
+        .parent("li")
+        .within(() => {
+          cy.contains(total).should("exist");
+        });
+    });
+  });
+
+  it("하우스를 클릭하면 중지 중인 하우스가 가동중으로 바뀌거나 가동중인 하우스가 중지됩니다.", () => {
+    cy.wait("@getFarmList");
+    farms.forEach((farm) => {
+      if (!farm.houses) {
+        return;
+      }
+      cy.contains(farm.name)
+        .parent()
+        .parent()
+        .within(() => {
+          farm.houses.forEach((house) => {
+            if (house.active) {
+              cy.contains(house.name)
+                .click()
+                .should("have.css", "backgroundColor", "rgb(255, 130, 105)")
+                .contains("중지");
+            } else {
+              cy.contains(house.name).click();
+
+              cy.contains(house.name)
+                .should("have.css", "backgroundColor", "rgb(122, 234, 156)")
+                .contains("가동중");
+            }
+          });
+        });
+    });
+  });
+});
+
+export {};
