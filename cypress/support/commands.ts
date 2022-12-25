621e759581d5d3e2c70cf39cc244ba32ead46608
Dongmin Ahn <eamon3481@naver.com>
2022-12-25 22:27:09 +0900

    test : login test 를 위해 custom command 추가

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -4,3 +4,12 @@ export const NAME = "안동민";
 export const ID = "eamon3481";
 export const CROP = "옥수수";
 export const FARM = "농장12";
+export const ACTIVE_COLOR = "rgb(122, 234, 156)";
+export const INACTIVE_COLOR = "rgb(255, 130, 105)";
+
+Cypress.Commands.add("login", () => {
+  cy.visit("/");
+  cy.get("input[name='name']").type(NAME);
+  cy.get("input[name='id']").type(ID);
+  cy.get("button[type='submit']").click();
+});
